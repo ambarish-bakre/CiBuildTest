ae42a4aa239b8dd9bfb6c7c62c285ad6b01d0970
Ambarish Bakre <ambarish.bakre@oneadvanced.com>
2018-02-05 14:36:07 +0530

    check if gulp is accessible when installed globally

--- versionbump.bat
+++ versionbump.bat
@@ -1,3 +1,3 @@
 cd VersionInfo
-set gulp=node_modules\.bin\gulp
-npm install && %gulp% setBuildVersion --buildno %BUILD_NUMBER% && %gulp% setAssemblyVersion && %gulp% setNpmPackageVersion && %gulp% setWixVersion
+@REM set gulp=node_modules\.bin\gulp
+npm install && gulp setBuildVersion --buildno %BUILD_NUMBER% && gulp setAssemblyVersion && gulp setNpmPackageVersion && gulp setWixVersion
